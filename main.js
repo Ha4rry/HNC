2a1c9ec144e284c6b5949d3e321996626dad34e2
ha4rry <harrymawn@gmail.com>
2023-10-22 09:51:12 +0100

    v1.499
    Mainly fixed  invalid characters, can still enter numbers like "+23" but that's not that bad........ v1.5 coming with a CSS update to experimental and main branches (live site)

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ function isDecimal(num) {
     return (num % 1 != 0);
 }
 
+function isCharIn(char, str, lowercase) { // use lowercase for only text, use none if checking specifically for symbols
+    let isIn = false;
+    if (lowercase == true) {
+        str = str.toLowerCase()
+        char = char.toLowerCase()
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) == char) {
+                isIn = true;
+            }
+        } 
+        return isIn;
+    }
+    else {
+        for (let i = 0; i < str.length; i++) {
+            if (str.charAt(i) == char) {
+                isIn = true;
+            }
+        } 
+        return isIn;
+    }
+}
 
 function isHappy() {
     let happy = false;
@@ -10,22 +31,25 @@ function isHappy() {
     let rawNumberInput = document.getElementById("numberInput").value;  
     let number = 0;
 
-    if (rawNumberInput.includes("e")) {
+    if (rawNumberInput == "") {
         allowCalculate = false;
     }
-    else if (rawNumberInput.includes("+")) {
+    else if (isCharIn("e", rawNumberInput, true)) {
+        allowCalculate = false;
+    }
+    else if (isCharIn("+", rawNumberInput, false)) {
         allowCalculate = false;
     }
     else if (Number(rawNumberInput)<0) {
         allowCalculate = false;
     }
-    else if (rawNumberInput.includes("-")) {
+    else if (isCharIn("-", rawNumberInput, false)) {
         allowCalculate = false;
     }
     else if (isDecimal(Number(rawNumberInput))) {
         allowCalculate = false;
     }
-    else if (rawNumberInput.includes('.')) {
+    else if (isCharIn(".", rawNumberInput, false)) {
         allowCalculate = false;
     }
     else {
@@ -73,27 +97,30 @@ function isHappy() {
         doCalc(number);
     }
     else if (!allowCalculate) {
-        if (rawNumberInput.includes("e")) {
+        if (rawNumberInput == "") {
+            alert('INPUT ERROR: This may be caused by attempting addition or subtraction. Also by entering random characters! e.g. "--", "1-"')   
+        }
+        else if (isCharIn("e", rawNumberInput, true)) {
             alert("Exponentials are not supported (yet)!");
         }
-        else if (rawNumberInput.includes("+")) {
+        else if (isCharIn("+", rawNumberInput, false)) { 
             alert("Addition not supported.");
         }
         else if (Number(rawNumberInput)<0) {
             alert("Negative numbers are not supported!");
         }
-        else if (rawNumberInput.includes("-")) {
+        else if (isCharIn("-", rawNumberInput, false)) {
             alert("Subtraction is not supported.");
         }
         else if (isDecimal(Number(rawNumberInput))) {
             alert("Decimals are not supported!");
         }
-        else if (rawNumberInput.includes(".")) {
+        else if (isCharIn(".", rawNumberInput, false)) {
             alert('Invalid character entered: ".", Decimals are not supported, Please try again.');
         }
         }
         else {
-        alert("ERROR: INVALID INPUT ERROR");
+            alert("ERROR: INVALID INPUT ERROR");
         }
     
 };
